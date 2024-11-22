9e608fd85f696bfe7c9d4b31183cf53653e6246b
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-22 21:42:30 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,12 +1,14 @@
 import math
 
+
 def area(a, b, c):
     if a < 0 or b < 0 or c < 0:
         raise ValueError("Sides of a triangle cannot be negative")
     if a + b <= c or a + c <= b or b + c <= a:
         raise ValueError("The given sides do not form a valid triangle")
-    s = (a + b + c) / 2  # Semi-perimeter
+    s = (a + b + c) / 2
     return math.sqrt(s * (s - a) * (s - b) * (s - c))
+
 
 def perimeter(a, b, c):
     if a < 0 or b < 0 or c < 0:
