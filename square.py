8196b982a0b11d9cde5405abd0887ec1e8bbb483
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-22 19:47:21 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,14 @@
+class Square:
+    def __init__(self, side_length):
+        if side_length <= 0:
+            raise ValueError("Side length must be a positive number.")
+        self.side_length = side_length
 
-def area(a):
-    return a * a
+    def area(self):
+        return self.side_length**2
 
+    def perimeter(self):
+        return 4 * self.side_length
 
-def perimeter(a):
-    return 4 * a
+    def __repr__(self):
+        return f"Square(side_length={self.side_length})"
