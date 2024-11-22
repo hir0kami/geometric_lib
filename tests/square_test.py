0775908cb815c27ba74310afbeb489736a9dbf73
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-22 21:38:32 +0300

    Update square_test.py

--- tests/square_test.py
+++ tests/square_test.py
@@ -1,39 +1,35 @@
 import unittest
-
-from square_lib import area, perimeter
+from square import area, perimeter
 
 
 class SquareTestCase(unittest.TestCase):
+    def test_zero_side(self):
+        side = 0
+        expected_area = 0
+        expected_perimeter = 0
+        self.assertEqual(area(side), expected_area)
+        self.assertEqual(perimeter(side), expected_perimeter)
+
+    def test_positive_side(self):
+        side = 4
+        expected_area = 16
+        expected_perimeter = 16
+        self.assertEqual(area(side), expected_area)
+        self.assertEqual(perimeter(side), expected_perimeter)
+
     def test_negative_side(self):
-        side = -1
+        side = -4
         with self.assertRaises(ValueError):
             area(side)
         with self.assertRaises(ValueError):
             perimeter(side)
 
-    def test_zero_side(self):
-        side = 0
-        expected_area = 0
-        expected_perimeter = 0
-
-        self.assertEqual(area(side), expected_area)
-        self.assertEqual(perimeter(side), expected_perimeter)
-
-    def test_positive_side(self):
-        side = 2
-        expected_area = 4
-        expected_perimeter = 8
-
-        self.assertEqual(area(side), expected_area)
-        self.assertEqual(perimeter(side), expected_perimeter)
-
-    def test_large_side(self):
-        side = 1e6
-        expected_area = side**2
-        expected_perimeter = 4 * side
-
-        self.assertEqual(area(side), expected_area)
-        self.assertEqual(perimeter(side), expected_perimeter)
+    def test_non_numeric_side(self):
+        side = "string"
+        with self.assertRaises(TypeError):
+            area(side)
+        with self.assertRaises(TypeError):
+            perimeter(side)
 
 
 if __name__ == "__main__":
