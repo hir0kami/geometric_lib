178c4e8ac7540fa0c5da3af930f16d6f6dffe5f0
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-22 19:46:21 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,26 @@
+import circle
+import square
+
+
+figs = ["circle", "square"]
+funcs = ["perimeter", "area"]
+sizes = {}
+
+
 import math
 
 
-def area(r):
-    return math.pi * r * r
+class Circle:
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius must be a positive number.")
+        self.radius = radius
 
+    def area(self):
+        return math.pi * self.radius**2
 
-def perimeter(r):
-    return 2 * math.pi * r
+    def perimeter(self):
+        return 2 * math.pi * self.radius
 
+    def __repr__(self):
+        return f"Circle(radius={self.radius})"
