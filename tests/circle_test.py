be9d1521bcc1fc72cda21a092409f004634b45ed
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-22 19:30:15 +0300

    Update circle_test.py

--- tests/circle_test.py
+++ tests/circle_test.py
@@ -1,22 +1,21 @@
-#circle_test
 import unittest
 import math
 import sys
+
 sys.path.append("..")
 
 from circle import area, perimeter
 
+
 class CircleTestCase(unittest.TestCase):
     def test_negative_radius(self):
-        """Тест на случай отрицательного радиуса."""
         radius = -1
-        with self.assertRaises(ValueError):  # Убедитесь, что выбрасывается ValueError
+        with self.assertRaises(ValueError):
             area(radius)
         with self.assertRaises(ValueError):
             perimeter(radius)
 
     def test_zero_radius(self):
-        """Тест на случай радиуса равного нулю."""
         radius = 0
         expected_area = 0
         expected_perimeter = 0
@@ -25,7 +24,6 @@ class CircleTestCase(unittest.TestCase):
         self.assertEqual(perimeter(radius), expected_perimeter)
 
     def test_positive_radius(self):
-        """Тест для положительного радиуса."""
         radius = 1
         expected_area = math.pi
         expected_perimeter = 2 * math.pi
@@ -34,13 +32,13 @@ class CircleTestCase(unittest.TestCase):
         self.assertAlmostEqual(perimeter(radius), expected_perimeter, places=7)
 
     def test_large_radius(self):
-        """Тест для большого радиуса."""
         radius = 1e6
-        expected_area = math.pi * (radius ** 2)
+        expected_area = math.pi * (radius**2)
         expected_perimeter = 2 * math.pi * radius
 
         self.assertAlmostEqual(area(radius), expected_area, places=7)
         self.assertAlmostEqual(perimeter(radius), expected_perimeter, places=7)
+
 
 if __name__ == "__main__":
     unittest.main()
