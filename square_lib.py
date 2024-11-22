29a32731aaf96e327b3da2bf052cd520fc44db64
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-22 21:39:57 +0300

    Update square_lib.py

--- square_lib.py
+++ square_lib.py
@@ -1,14 +1,9 @@
-class Square:
-    def __init__(self, side_length):
-        if side_length <= 0:
-            raise ValueError("Side length must be a positive number.")
-        self.side_length = side_length
+def area(side):
+    if side < 0:
+        raise ValueError("Side length cannot be negative")
+    return side * side
 
-    def area(self):
-        return self.side_length**2
-
-    def perimeter(self):
-        return 4 * self.side_length
-
-    def __repr__(self):
-        return f"Square(side_length={self.side_length})"
+def perimeter(side):
+    if side < 0:
+        raise ValueError("Side length cannot be negative")
+    return 4 * side
