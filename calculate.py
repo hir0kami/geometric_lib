5224f8a07162946b9f3a446ce4d4f5724fcf2881
Artyom Kondratenko <466248@niuitmo.ru>
2024-11-30 16:27:32 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -20,11 +20,13 @@ def calc(fig, func, size):
                 else f"math.pi * {size[0]}**2"
             )
         elif fig == "square":
-            expression = f"4 * {size[0]}" if func == "perimeter" else f"{size[0]}**2"
-        result = eval(expression)  # Использование eval для вычисления
+            expression = f"4 * {size[0]}" if func == "perimeter" 
+            else f"{size[0]}**2"
+        result = eval(expression)
         return result
     except Exception as e:
-        raise ValueError(f"Error calculating {func} for {fig} with size {size}: {e}")
+        raise ValueError(f"Error calculating {func} 
+        for {fig} with size {size}: {e}")
 
 
 if __name__ == "__main__":
@@ -33,10 +35,12 @@ if __name__ == "__main__":
     size = []
 
     while fig not in figs:
-        fig = input(f"Enter figure name, available options are {figs}: ").strip()
+        fig = input(f"Enter figure name,
+        available options are {figs}: ").strip()
 
     while func not in funcs:
-        func = input(f"Enter function name, available options are {funcs}: ").strip()
+        func = input(f"Enter function name,
+        available options are {funcs}: ").strip()
 
     expected_size_count = sizes.get(fig, 1)
     while len(size) != expected_size_count:
