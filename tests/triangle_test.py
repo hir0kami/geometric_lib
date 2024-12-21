98ce0f9df84fb9040c910965d0c440dca01a7b90
Artyom Kondratenko <466248@niuitmo.ru>
2024-12-21 22:38:58 +0300

    Update triangle_test.py

--- tests/triangle_test.py
+++ tests/triangle_test.py
@@ -1,5 +1,8 @@
 import unittest
+import sys
 from triangle import area, perimeter
+
+sys.path.append("..")
 
 
 class TriangleTestCase(unittest.TestCase):
@@ -7,6 +10,7 @@ class TriangleTestCase(unittest.TestCase):
         a, b, c = 0, 0, 0
         with self.assertRaises(ValueError):
             area(a, b, c)
+
         with self.assertRaises(ValueError):
             perimeter(a, b, c)
 
@@ -14,13 +18,15 @@ class TriangleTestCase(unittest.TestCase):
         a, b, c = 3, 4, 5
         expected_area = 6
         expected_perimeter = 12
-        self.assertAlmostEqual(area(a, b, c), expected_area, places=5)
+
+        self.assertAlmostEqual(area(a, b, c), expected_area)
         self.assertEqual(perimeter(a, b, c), expected_perimeter)
 
-    def test_invalid_triangle(self):
+    def test_invalid_triangle_inequality(self):
         a, b, c = 1, 2, 10
         with self.assertRaises(ValueError):
             area(a, b, c)
+
         with self.assertRaises(ValueError):
             perimeter(a, b, c)
 
@@ -28,17 +34,8 @@ class TriangleTestCase(unittest.TestCase):
         a, b, c = -3, 4, 5
         with self.assertRaises(ValueError):
             area(a, b, c)
+
         with self.assertRaises(ValueError):
-            perimeter(a, b, c)
-
-    def test_non_numeric_sides_area(self):
-        a, b, c = "string", 4, 5
-        with self.assertRaises(TypeError):
-            area(a, b, c)
-
-    def test_non_numeric_sides_perimeter(self):
-        a, b, c = "string", 4, 5
-        with self.assertRaises(TypeError):
             perimeter(a, b, c)
 
 
